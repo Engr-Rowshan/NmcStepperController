b773ba19bb607f3e08e6beb3e643619b3b06990d
Engr. Syed Rowshan Ali <engr.rowshan@gmail.com>
2017-09-21 22:14:43 +0600

    Stepper Controler Library at initial

---
+++ NmcStepperController/NmcStepperController.cpp
@@ -0,0 +1,10 @@
+/*
+ Name:		NmcStepperController.cpp
+ Created:	9/21/2017 9:56:41 PM
+ Author:	engrr
+ Editor:	http://www.visualmicro.com
+*/
+
+#include "NmcStepperController.h"
+
+
---
+++ NmcStepperController/NmcStepperController.h
@@ -0,0 +1,19 @@
+/*
+ Name:		NmcStepperController.h
+ Created:	9/21/2017 9:56:41 PM
+ Author:	engrr
+ Editor:	http://www.visualmicro.com
+*/
+
+#ifndef _NmcStepperController_h
+#define _NmcStepperController_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+
+#endif
+
